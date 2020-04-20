6ed44bb4f8d1391192537a5f99e8917818360e89
VaderSteve76 <alverson.s@yahoo.com>
2020-04-19 21:24:44 -0400

    Added getElementsById

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.getElementById('container');
+const seats = document.getElementById('.row .seat:not(.occupied)')
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
